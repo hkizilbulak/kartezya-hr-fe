8cc220e6dcc8285fb4960a6c8a8a548be79e0921
musaay <musaay11@gmail.com>
2026-08-06 00:42:11 +0300

    fix: pass X-API-Key statically in axios create to fix interceptor bug

--- helpers/api/cvSearchAxiosInstance.ts
+++ helpers/api/cvSearchAxiosInstance.ts
@@ -6,21 +6,9 @@ const cvSearchAxiosInstance = axios.create({
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
+    'X-API-Key': process.env.NEXT_PUBLIC_CV_API_KEY || '',
   },
   timeout: 60000, // CV processing can take time
 })
-
-cvSearchAxiosInstance.interceptors.request.use(
-  (config) => {
-    const cvApiKey = process.env.NEXT_PUBLIC_CV_API_KEY;
-    if (cvApiKey) {
-      config.headers['X-API-Key'] = cvApiKey;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export default cvSearchAxiosInstance
